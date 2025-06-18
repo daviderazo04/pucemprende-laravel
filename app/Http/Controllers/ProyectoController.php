a322a342da7a6945e32bf27c9c54bac2010ec6a4
Nicolás Guevara <nicoguevara0420@gmail.com>
2025-06-18 15:41:44 -0500

    Cambiando API de proyectos para que acepte un valor de null en equipo_id si fuese necesario

--- app/Http/Controllers/ProyectoController.php
+++ app/Http/Controllers/ProyectoController.php
@@ -32,7 +32,7 @@ class ProyectoController extends Controller
         }
 
         $validator = Validator::make($request->all(), [
-            'equipo_id' => 'required|integer|exists:equipos,id',
+            'equipo_id' => 'nullable|integer|exists:equipos,id',
             'titulo' => 'required|string|max:50',
             'descripcion' => 'required|string|max:1000',
             'estado' => 'required|string|max:20',
