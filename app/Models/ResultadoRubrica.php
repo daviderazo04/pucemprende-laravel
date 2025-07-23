ed42a90919ad008f58386bc92376548b36e71f69
Nicolás Guevara <nicoguevara0420@gmail.com>
2025-07-22 20:08:44 -0500

    Modelo de ResultadoRubrica creado

---
+++ app/Models/ResultadoRubrica.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+/**
+ * Class ResultadoRubrica
+ * 
+ * @property int $id
+ * @property int|null $persona_id
+ * @property int|null $plantilla_id
+ * @property int|null $equipo_id
+ * @property float|null $total
+ * 
+ * @property Persona|null $persona
+ * @property PlantillaEvaluacion|null $plantilla
+ * @property Equipo|null $equipo
+ */
+class ResultadoRubrica extends Model
+{
+    protected $table = 'resultadoRubrica';
+    public $timestamps = false;
+
+    protected $casts = [
+        'persona_id' => 'int',
+        'plantilla_id' => 'int',
+        'equipo_id' => 'int',
+        'total' => 'float'
+    ];
+
+    protected $fillable = [
+        'persona_id',
+        'plantilla_id',
+        'equipo_id',
+        'total'
+    ];
+
+    public function persona(): BelongsTo
+    {
+        return $this->belongsTo(Persona::class);
+    }
+
+    public function plantilla(): BelongsTo
+    {
+        return $this->belongsTo(PlantillaEvaluacion::class, 'plantilla_id');
+    }
+
+    public function equipo(): BelongsTo
+    {
+        return $this->belongsTo(Equipo::class);
+    }
+}
